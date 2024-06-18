cbf317d5deef63dda53b8e0cfc48b8901964e5cd
Hargun3 <hargunsingh312@gmail.com>
2024-06-17 21:10:04 -0400

    updatedfix

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def login():
 
     }
 
-    auth_url  = f"{AUTH_URL} ? {urllib.parse.urlencode(params)}"
+    auth_url  = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
 
     return redirect(auth_url)
 
@@ -48,10 +48,10 @@ def callback():
         return jsonify({"error": request.args['error']})
     
 
-    if 'code' in requests.args:
+    if 'code' in request.args:
         req_body  = {
-            'code': requests.args['code'],
-            'grant_type' : 'authorization_Code',
+            'code': request.args['code'],
+            'grant_type' : 'authorization_code',
             'redirect_uri' : REDIRECT_URI,
             'client_id' : CLIENT_ID,
             'client_secret' : CLIENT_SECRET
@@ -75,14 +75,14 @@ def get_playlists():
     if 'access_token' not in session:
         return redirect('login')
     
-    if datetime.now().timestamp > session['expires_at']:
+    if datetime.now().timestamp() > session['expires_at']:
         return redirect('/refresh-token')
     
     headers = {
         'Authorization' : f"Bearer {session['access_token']}"
     }
 
-    response = requests.get(API_BASE_URL + 'me\playlists', headers=headers)
+    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
     playlists = response.json()
 
     return jsonify(playlists)
@@ -94,7 +94,7 @@ def refresh_token():
     if 'refresh_token' not in session:
         return redirect('/login')
     
-    if datetime.now().timestamp > session['expires_at']:
+    if datetime.now().timestamp() > session['expires_at']:
         req_body = {
             'grant_type' : 'refresh_token',
             'refresh_token' : session['refresh_token'],
@@ -106,7 +106,7 @@ def refresh_token():
         response = requests.post(TOKEN_URL, data=req_body)
         new_token_info = response.json()
 
-        session['access_token'] = new_token_info['acess_token']
+        session['access_token'] = new_token_info['access_token']
         session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in'] 
 
 
